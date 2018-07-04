380859f7c6ed8b6cebf466b70651153e2c410ca0
ramdhanselang <40796624+ramdhanselang@users.noreply.github.com>
2018-07-04 20:27:29 +0700

    user_model dan login ctrl

--- ProjectUAS/application/controllers/Login.php
+++ ProjectUAS/application/controllers/Login.php
@@ -5,9 +5,52 @@ class Login extends CI_Controller {
 
 	public function index()
 	{
+		$this->load->library('form_validation');
+		$this->$this->form_validation->set_rules('UsernameUser', 'Username', 'trim|required|');
+		$this->$this->form_validation->set_rules('PasswordUser', 'Password', 'trim|required|');
+		if($this->form_validation->run() == FALSE){
+			$this->load->view('login');
+		}else{
+			if($this->$this->session->userdata('logged_in')['levelUser'] ==  'admin'){
+				redirect('Home/Admin','refresh');
+			}else{
+				redirect('Home','refresh');
+			}
+		}
 		$this->load->view('login');
+	
 	}
 
+	public function logout()
+        {
+            # code...
+            $this->session->unset_userdata('logged_in');
+            $this->session->sess_destroy();
+            redirect('login','refresh');
+        }
+
+	public function cekDb($password)
+        {
+            # code...
+            $this->load->model('user_model');
+            $username = $this->input->post('usernameUser'); 
+            $result = $this->user->login($username,$password);
+            if($result){
+                $session_array = array();
+                foreach ($result as $key) {
+                    $session_array = array(
+                        'idUser'=>$key->idUser,
+                        'usernameUser'=>$key->usernameUser,
+                        'levelUser' => $row->levelUser
+                    );
+                    $this->session->set_userdata('logged_in',$session_array);
+                }
+                return true;
+            }else{
+                $this->form_validation->set_message('cekDb',"login gagal");
+                return false;
+            }
+        }
 }
 
 /* End of file home.php */
--- ProjectUAS/application/models/user_model.php
+++ ProjectUAS/application/models/user_model.php
@@ -9,7 +9,30 @@
 			parent::__construct();
 		}
 
-		
+		public function login($username,$password)
+        {
+            $this->db->select('idUser,usernameUser,passwordUser,levelUser');
+            $this->db->from('user');
+            $this->db->where('usernameUser',$username);
+            $this->db->where('passwordUser', MD5($password));
+            $query = $this->db->get();
+            if($query->num_rows()==1){
+                return $query->result();
+            }else{
+                return false;
+            }
+            
+        }
+     
+        public function insert()
+        {
+            $data = array(
+                'usernameUser' => $this->input->post('usernameUser'),
+                'passwordUser' => md5($this->input->post('passwordUser')).
+                'levelUser' => $this->input->post('levelUser')
+            );
+            $this->db->insert('user',$data);
+        }
 	}
 
 /* End of file user_model.php */
