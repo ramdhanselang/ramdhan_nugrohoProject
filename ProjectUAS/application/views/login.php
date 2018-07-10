5b5cde33f13efa1eb3627e12d2eee8652146dbb9
ramdhanselang <40796624+ramdhanselang@users.noreply.github.com>
2018-07-10 21:54:50 +0700

    login

--- ProjectUAS/application/views/login.php
+++ ProjectUAS/application/views/login.php
@@ -7,14 +7,15 @@
      <body>
         <main role="main" class="container">
       <div class="jumbotron">
+        <?php echo form_open('login'); ?>
         <center><h1><font color="white">SIAKAD</font> <font color="#00cccc"x>PROJECT</font></b></center><br>
          <font color="white"><?php echo validation_errors()?></font> 
             <div class="form-group">
-            <input type="text" class="form-control" id="username" name="username" placeholder="username">
+            <input type="text" class="form-control" id="username" name="usernameUser" placeholder="username">
             </div>
 
             <div class="form-group">
-            <input type="password" class="form-control" id="password" name="password" placeholder="password">
+            <input type="password" class="form-control" id="password" name="passwordUser" placeholder="password">
             </div>
 
             <center><button type="submit" class="btn btn-lg btn-success">LOGIN</button></center>
